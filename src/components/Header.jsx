b9e3099f7d47e17af34001ee937edb360ca30bc7
Johnnie Lopez <johesloc@gmail.com>
2021-08-02 17:30:35 -0500

    useContext setup and use to change a color text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import '../assets/styles/components/Header.css'
+import ThemeContext from '../context/ThemeContext'
+
 // Funcion que permite estblecer el light mode y el dark mode
 
 // el estado inicial de darkmode es false
 
 const Header = () => {
   const [darkMode, setDarkMode] = useState(false)
+  const color = useContext(ThemeContext)
 
   const darkmode = (isdarkMode) => {
     if (!isdarkMode) {
       document.querySelector('#App').style.background = 'black'
-      document.querySelector('h1').style.color = 'white'
+      // document.querySelector('h1').style.color = 'white'
     }
     if (isdarkMode) {
       document.querySelector('#App').style.background = 'white'
-      document.querySelector('h1').style.color = 'black'
+      // document.querySelector('h1').style.color = 'black'
     }
   }
 
@@ -24,7 +27,7 @@ const Header = () => {
   }
   return (
     <div className="Header">
-      <h1>ReactHooks</h1>
+      <h1 style={{ color }}>ReactHooks</h1>
       <button type="button" onClick={handleClick}>
         {darkMode ? 'Dark Mode' : 'Light Mode'}
       </button>
