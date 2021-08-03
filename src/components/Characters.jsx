771e3a667a80343bdc417251c927d0f33e540191
Johnnie Lopez <johesloc@gmail.com>
2021-08-02 20:02:23 -0500

    Use tof useReducer to work with the state, add characters in the Favorite sections

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,9 +1,61 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useState, useEffect, useContext, useReducer } from 'react'
 import '../assets/styles/components/Characters.css'
 import ThemeContext from '../context/ThemeContext'
 
+// para usar useReducer vamos a tener un estado inicial
+const initialState = {
+  favorite: [],
+}
+
+const favoriteReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_FAVORITE':
+      return {
+        ...state,
+        favorite: [...state.favorite, action.payload],
+      }
+    default:
+      return state
+  }
+}
+
+const CharacterItems = ({ theme, characters, handleClick, hidden, title }) =>
+  characters.length > 0 && (
+    <>
+      <h2
+        style={{ color: `${theme ? 'black' : 'white'}` }}
+        className={`${theme ? 'lightmode' : 'darkmode'}`}
+      >
+        {title}
+      </h2>
+      <div className={`Characters ${theme ? 'lightmode' : 'darkmode'}`}>
+        {characters.map((character) => (
+          <article className="Card-container" key={character.id}>
+            <img
+              className="Card-image"
+              src={character.image}
+              alt={character.name}
+            />
+            <h3 className="Card-name">{character.name}</h3>
+            <button
+              type="button"
+              hidden={hidden}
+              onClick={() => handleClick(character)}
+            >
+              add to Favorite
+            </button>
+          </article>
+        ))}
+      </div>
+    </>
+  )
+
 const Characters = () => {
   const [characters, setCharacters] = useState([])
+  const { theme } = useContext(ThemeContext)
+  // al useReducer se pasa el reducer y el initialState
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState)
+
   useEffect(() => {
     fetch('https://rickandmortyapi.com/api/character')
       .then((response) => response.json())
@@ -20,21 +72,29 @@ const Characters = () => {
     // fetchData()
   }, [])
 
-  const { theme } = useContext(ThemeContext)
+  const handleClick = (favorite) => {
+    dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
+  }
+
+  console.log('Favoritos: ', favorites.favorite)
 
   return (
-    <div className={`Characters ${theme ? 'lightmode' : 'darkmode'}`}>
-      {characters.map((character) => (
-        <article className="Card-container" key={character.id}>
-          <img
-            className="Card-image"
-            src={character.image}
-            alt={character.name}
-          />
-          <h2 className="Card-name">{character.name}</h2>
-        </article>
-      ))}
-    </div>
+    <>
+      <CharacterItems
+        theme={theme}
+        characters={favorites.favorite}
+        handleClick={handleClick}
+        hidden={true}
+        title="Favorites"
+      />
+      <CharacterItems
+        theme={theme}
+        characters={characters}
+        handleClick={handleClick}
+        hidden={false}
+        title="Characters"
+      />
+    </>
   )
 }
 
